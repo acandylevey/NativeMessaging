48e32e4b31d5e43363e2419a1cc0253afe690626
Olivier PIRLET <olivier@jereck.net>
2020-06-23 17:40:57 +0200

    - Added test application

---
+++ NativeMessagingTest/MyHost.cs
@@ -0,0 +1,23 @@
+﻿using NativeMessaging;
+using Newtonsoft.Json.Linq;
+
+namespace NativeMessagingTest {
+    public class MyHost : Host {
+        // This can currently be used with the example app provided here:
+        // https://chromium.googlesource.com/chromium/src/+/master/chrome/common/extensions/docs/examples/api/nativeMessaging
+
+        private const bool SendConfirmationReceipt = true;
+
+        public override string Hostname {
+            get { return "com.google.chrome.example.echo"; }
+        }
+
+        public MyHost() : base(SendConfirmationReceipt) {
+
+        }
+
+        protected override void ProcessReceivedMessage(JObject data) {
+            SendMessage(data);
+        }
+    }
+}
