eb57a595d8cb016adf9ed2d1bedd2ae3f68f702a
Olivier PIRLET <olivier@jereck.net>
2020-06-23 16:44:29 +0200

    - Updated AssemblyInfo - Updated XML Comments - Added Nuspec file to create a NuGet package

--- NativeMessaging/Exceptions.cs
+++ NativeMessaging/Exceptions.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace NativeMessaging
 {
+    /// <summary>
+    /// Exception raised when trying to interact with chrome while the extension is not registered in the Windows Registry
+    /// </summary>
     [Serializable]
     public class NotRegisteredWithChromeException : Exception
     {
-        public NotRegisteredWithChromeException() { }
-        public NotRegisteredWithChromeException(string message) : base(message) { }
-        public NotRegisteredWithChromeException(string message, Exception inner) : base(message, inner) { }
-        protected NotRegisteredWithChromeException(
+        internal NotRegisteredWithChromeException() { }
+        internal NotRegisteredWithChromeException(string message) : base(message) { }
+        internal NotRegisteredWithChromeException(string message, Exception inner) : base(message, inner) { }
+        internal NotRegisteredWithChromeException(
           System.Runtime.Serialization.SerializationInfo info,
           System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
